793f0fe5138997fb1f853294111117c0d2b10e45
Glitch (greet-bot) <none>
2022-12-16 13:35:13 +0000

    🎣🚤 Checkpoint ./index.js:52236073/159

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const store = require('./store');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken:process.env.SOCKET_TOKEN,
+  socketMode: true,
 });
 
 
-app.event('app_home_opened', async ({ event, say }) => {
+app.event('app_mention', async ({ event, say }) => {
   // Look up the user from DB
   let user = store.getUser(event.user);
-
+  console.log("dendenedSne")
   if (!user) {
     user = {
       user: event.user,
@@ -28,6 +30,6 @@ app.event('app_home_opened', async ({ event, say }) => {
 // Start your app
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  console.log('⚡️ Bolt app is runningggg!');
 })();
 
